78ce5a7cdaa032d4fd02c1c2c72a4b09c19e7425
Kolja@netcup <Kolja.Windeler@gmail.com>
2016-09-08 12:03:26 +0200

    ?

--- a_common_scripts/db.php
+++ a_common_scripts/db.php
@@ -1,8 +1,8 @@
 <?php
 #############################Sql Login################
-$db='database';
-$db_user = 'user';
-$db_pw = 'password';
+$db='akaweb';
+$db_user = 'akaweb';
+$db_pw = 'GNaZr8xbCUxy4UQe';
 $host = 'localhost';
 #############################Sql Login################
 ?>
